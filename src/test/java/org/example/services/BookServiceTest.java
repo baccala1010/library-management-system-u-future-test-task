ef071fc66b80cde617653bf1bed4e7c07249155f
bbacc <231388@astanait.edu.kz>
2024-07-01 16:29:49 +0500

    test

--- src/test/java/org/example/services/BookServiceTest.java
+++ src/test/java/org/example/services/BookServiceTest.java
@@ -18,26 +18,6 @@ public class BookServiceTest {
     }
 
     @Test
-    public void testGetAllBooks() {
-        Book book1 = new Book("9780553149661", "The Catcher in the Rye", "J.D. Salinger", 1951);
-        Book book2 = new Book("9780140283297", "To Kill a Mockingbird", "Harper Lee", 1960);
-        Book book3 = new Book("9780023042706", "The Prince", "Niccolo Machiavelli", 1976);
-        Book book4 = new Book("9780140439199", "The Art of War", "Sun Tzu", 2003);
-        bookService.addBook(book1);
-        bookService.addBook(book2);
-        bookService.addBook(book3);
-        bookService.addBook(book4);
-
-        List<Book> allBooks = bookService.getAllBooks();
-        assertNotNull(allBooks);
-        assertEquals(4, allBooks.size());
-        assertEquals(book1, allBooks.get(0));
-        assertEquals(book2, allBooks.get(1));
-        assertEquals(book3, allBooks.get(2));
-        assertEquals(book4, allBooks.get(3));
-    }
-
-    @Test
     public void testAddBook() {
         Book book = new Book("9780553149661", "The Catcher in the Rye", "J.D. Salinger", 1951);
         bookService.addBook(book);
@@ -69,45 +49,44 @@ public class BookServiceTest {
         bookService.addBook(book4);
 
         //  search criteria:
-        //  1 - title
-        //  2 - author
-        //  3 - publication year
-        //  4 - isbn
+        //  title
+        //  author
+        //  publication year
+        //  isbn
 
         // Search by title
-        List<Book> searchResults = bookService.searchBooks(1, "The Catcher in the Rye");
-        assertEquals(1, searchResults.size());
-        assertEquals(book1, searchResults.get(0));
+        searchingTestFound("title", "The Catcher in the Rye", book1);
 
         // Search by author
-        searchResults = bookService.searchBooks(2, "Harper Lee");
-        assertEquals(1, searchResults.size());
-        assertEquals(book2, searchResults.get(0));
+        searchingTestFound("author", "Harper Lee", book2);
 
         // Search by publication year
-        searchResults = bookService.searchBooks(3, "1976");
-        assertEquals(1, searchResults.size());
-        assertEquals(book3, searchResults.get(0));
+        searchingTestFound("publication year", "1976", book3);
 
         // Search by ISBN
-        searchResults = bookService.searchBooks(4, "9780140439199");
-        assertEquals(1, searchResults.size());
-        assertEquals(book4, searchResults.get(0));
+        searchingTestFound("isbn", "9780140439199", book4);
 
         // Search by title with no match
-        searchResults = bookService.searchBooks(1, "Nonexistent Book");
-        assertTrue(searchResults.isEmpty());
+        searchingTestNotFound("title", "Nonexistent Book");
 
         // Search by author with no match
-        searchResults = bookService.searchBooks(2, "Unknown Author");
-        assertTrue(searchResults.isEmpty());
+        searchingTestNotFound("author", "Unknown Author");
 
         // Search by publication year with no match
-        searchResults = bookService.searchBooks(3, "9999");
-        assertTrue(searchResults.isEmpty());
+        searchingTestNotFound("publication year", "1333");
 
         // Search by ISBN with no match
-        searchResults = bookService.searchBooks(4, "0000000000000");
+        searchingTestNotFound("isbn", "456789087654");
+    }
+
+    private void searchingTestFound(String criteria, String value, Book book) {
+        List<Book> searchResults = bookService.searchBooks(criteria, value);
+        assertEquals(1, searchResults.size());
+        assertEquals(book, searchResults.get(0));
+    }
+
+    private void searchingTestNotFound(String criteria, String value) {
+        List<Book> searchResults = bookService.searchBooks(criteria, value);
         assertTrue(searchResults.isEmpty());
     }
 }
